7f4467d7b9c426d61a08346009ccdf72b1073307
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2023-03-28 14:22:17 -0700

    start background

--- homelga_app/lib/start_page.dart
+++ homelga_app/lib/start_page.dart
@@ -6,57 +6,68 @@ class StartPage extends StatelessWidget {
   const StartPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            Color(0xFF1F7961),
+            Color.fromARGB(255, 183, 198, 106),
+          ],
+        )),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('images/logo.png'),
-            const SizedBox(height: 30.0),
-            Image.asset('images/tree.png'),
-            const SizedBox(height: 30.0),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return LoginPage();
-                      },
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF1F7961),
-                    minimumSize: const Size(333.0, 60.0),
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-                child: const Text('Login',
-                    style: TextStyle(
-                        fontSize: 36.0,
-                        fontFamily: 'Playfair',
-                        fontWeight: FontWeight.w500))),
-            const SizedBox(height: 20.0),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return CreatePage();
-                      },
-                    ),
-                  );
-                },
-                child: const Text('New teacher? Create an account',
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.white,
-                        fontFamily: 'Playfair',
-                        fontWeight: FontWeight.w500,
-                        decoration: TextDecoration.underline)))
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/logo.png'),
+                const SizedBox(height: 30.0),
+                Image.asset('images/tree.png'),
+                const SizedBox(height: 30.0),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return LoginPage();
+                          },
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1F7961),
+                        minimumSize: const Size(333.0, 60.0),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)))),
+                    child: const Text('Login',
+                        style: TextStyle(
+                            fontSize: 36.0,
+                            fontFamily: 'Playfair',
+                            fontWeight: FontWeight.w500))),
+                const SizedBox(height: 20.0),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return CreatePage();
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text('New teacher? Create an account',
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                            fontFamily: 'Playfair',
+                            fontWeight: FontWeight.w500,
+                            decoration: TextDecoration.underline)))
+              ],
+            ),
           ],
-        ),
-      ],
-    );
+        ));
   }
 }
