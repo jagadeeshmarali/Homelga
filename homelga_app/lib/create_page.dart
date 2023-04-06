cdbc0b44f18b9212449e527a67d98e03db9a91d2
ddupree1 <ddupree2@scu.edu>
2023-04-05 20:16:33 -0700

    create account error messages

--- homelga_app/lib/create_page.dart
+++ homelga_app/lib/create_page.dart
@@ -142,6 +142,7 @@ class CreatePage extends StatelessWidget {
                     const SizedBox(height: 40.0),
                     ElevatedButton(
                         onPressed: () async {
+                          var errorMessage = '';
                           DatabaseReference userRef =
                               userDatabase.ref("users/$id");
                           try {
@@ -158,12 +159,104 @@ class CreatePage extends StatelessWidget {
                                     builder: (context) => const TeacherHome()));
                           } on FirebaseAuthException catch (e) {
                             if (e.code == 'weak-password') {
-                              print('The password provided is too weak.');
+                              errorMessage =
+                                  'The password provided is too weak.';
                             } else if (e.code == 'email-already-in-use') {
-                              print(
-                                  'The account already exists for that email.');
+                              errorMessage =
+                                  'The account already exists for that email.';
+                            } else {
+                              errorMessage =
+                                  'There was an error creating your account. Please try again!';
                             }
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              backgroundColor: Colors.transparent,
+                              behavior: SnackBarBehavior.floating,
+                              elevation: 0,
+                              content: Stack(
+                                alignment: Alignment.center,
+                                clipBehavior: Clip.none,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(8),
+                                    height: 70,
+                                    decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 183, 198, 106),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(15)),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        const SizedBox(
+                                          width: 48,
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const Text(
+                                                'Oops Error!',
+                                                style: TextStyle(
+                                                    fontSize: 18,
+                                                    color: Colors.white),
+                                              ),
+                                              Text(
+                                                errorMessage,
+                                                style: const TextStyle(
+                                                    fontSize: 14,
+                                                    color: Colors.white),
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Positioned(
+                                      bottom: 25,
+                                      left: 20,
+                                      child: ClipRRect(
+                                        child: Stack(
+                                          children: const [
+                                            Icon(
+                                              Icons.circle,
+                                              color: Color(0xFF1F7961),
+                                              size: 17,
+                                            )
+                                          ],
+                                        ),
+                                      )),
+                                  Positioned(
+                                      top: -20,
+                                      left: 5,
+                                      child: Stack(
+                                        alignment: Alignment.center,
+                                        children: [
+                                          Container(
+                                            height: 30,
+                                            width: 30,
+                                            decoration: const BoxDecoration(
+                                              color: Color(0xFF1F7961),
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(15)),
+                                            ),
+                                          ),
+                                          const Positioned(
+                                              top: 5,
+                                              child: Icon(
+                                                Icons.clear_outlined,
+                                                color: Colors.white,
+                                                size: 20,
+                                              ))
+                                        ],
+                                      )),
+                                ],
+                              ),
+                            ));
                           } catch (e) {
+                            //errorMessage = String(e);
                             print(e);
                           }
                         },
