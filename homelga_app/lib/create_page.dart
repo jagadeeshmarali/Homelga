0be29685fc985b885ed320172e995fde0820ec06
ddupree1 <ddupree2@scu.edu>
2023-04-11 13:29:54 -0700

    students added again

--- homelga_app/lib/create_page.dart
+++ homelga_app/lib/create_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'teacher_home.dart';
@@ -156,6 +158,14 @@ class CreatePage extends StatelessWidget {
                               "students": {},
                               "assignments": {}
                             });
+                            DatabaseReference students =
+                                userRef.child("students");
+                            DatabaseEvent event = await students.once();
+                            final studentList =
+                                jsonEncode(event.snapshot.value);
+                            final parsedStudentList = jsonDecode(studentList);
+                            parsedStudentList.forEach(
+                                (k, v) => studentNames.add(v["studentName"]));
                             if (nameCtrl.text.contains(' ') &&
                                 emailCtrl.text.contains('@')) {
                               Navigator.push(
